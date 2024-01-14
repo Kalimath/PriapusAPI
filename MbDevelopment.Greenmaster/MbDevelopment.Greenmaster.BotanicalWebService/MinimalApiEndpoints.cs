49b52335bc9016e0cec8d8b3e16ddc86a556a5a1
mathieu <mathieu_broe@yahoo.com>
2024-01-14 15:18:12 +0100

    minor improvements

--- MbDevelopment.Greenmaster/MbDevelopment.Greenmaster.BotanicalWebService/MinimalApiEndpoints.cs
+++ MbDevelopment.Greenmaster/MbDevelopment.Greenmaster.BotanicalWebService/MinimalApiEndpoints.cs
@@ -1,8 +1,3 @@
-using MbDevelopment.Greenmaster.Contracts.WebApi;
-using MbDevelopment.Greenmaster.Core.Botanical;
-using MbDevelopment.Greenmaster.DataAccess;
-using Microsoft.EntityFrameworkCore;
-
 namespace MbDevelopment.Greenmaster.BotanicalWebService;
 
 public static class MinimalApiEndpoints
